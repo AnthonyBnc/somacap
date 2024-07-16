5d0a5fd57c4e65eb3090ffb6a02473d5b8eb7661
Anthony <160201522+AnthonyBnc@users.noreply.github.com>
2024-07-17 00:51:06 +1000

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,17 +7,17 @@ import LandingPage from "./_components/somaCap/landingtest";
 export default function Home() {
   return (
     <>
-      <div className="relative w-fit">
-        <div className="relative w-full">
+      <div className="relative w-full">
+        <div className="relative w-fit sm:w-full">
           <LandingPage />
-          <div className="relative px-4">
+          <div className="relative mx-auto px-4">
             <div className="relative mx-auto min-h-screen md:max-w-6xl">
               <BlogArticle />
             </div>
           </div>
           <div className="mb-5 flex items-center justify-center">
             <button className="hidden items-center justify-center rounded-lg bg-deepPurple px-4 py-2 font-semibold text-white shadow-md hover:bg-purple-800 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 md:flex">
-              More Articles
+              More Article
             </button>
           </div>
           <Footer />
